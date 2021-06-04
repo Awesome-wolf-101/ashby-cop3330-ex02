536aee6517bb809e8057531cd303454b0b17baa8
Joshua Ashby <joshuaashby16@gmail.com>
2021-06-04 15:50:11 -0400

    Initial commit

--- src/main/java/APP.java
+++ src/main/java/APP.java
@@ -1,5 +1,16 @@
+
+import java.util.Scanner;
+
 public class APP {
-    public static void main(String args[]){
-        System.out.println("Hello Java");
+    public static void main(String[] args){
+            System.out.println("What is the input string?");
+        Scanner sc = new Scanner(System.in);
+        String InputString = sc.nextLine();
+        int letters = InputString.length();
+        System.out.print(InputString);
+        System.out.print(" has "+ letters);
+        System.out.print(" characters");
+
+
     }
 }
