5d8848863ed2efeef3d08169214c57c7b846cd5e
Joshua Ashby <joshuaashby16@gmail.com>
2021-06-06 21:36:32 -0400

    Initial commit

--- src/main/java/APP.java
+++ src/main/java/APP.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 
 public class APP {
     public static void main(String[] args){
-            System.out.println("What is the input string?");
+        System.out.println("What is the input string?");
         Scanner sc = new Scanner(System.in);
         String InputString = sc.nextLine();
+        String OutPutString = InputString;
         int letters = InputString.length();
-        System.out.print(InputString);
-        System.out.print(" has "+ letters);
-        System.out.print(" characters");
+        OutPutString += " has " + letters;
+        OutPutString += " characters.";
+        System.out.print(OutPutString);
 
 
     }
